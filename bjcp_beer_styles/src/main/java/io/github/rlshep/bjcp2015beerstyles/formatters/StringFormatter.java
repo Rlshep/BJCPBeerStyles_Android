49ab585687945477eafa560bd6a94807b49a0ada
Richard Sheppard <Rlshep@gmail.com>
2015-08-17 20:40:10 -0400

    Fixed highlight capitalization bug.

--- bjcp_beer_styles/src/main/java/io/github/rlshep/bjcp2015beerstyles/formatters/StringFormatter.java
+++ bjcp_beer_styles/src/main/java/io/github/rlshep/bjcp2015beerstyles/formatters/StringFormatter.java
@@ -9,14 +9,39 @@ public class StringFormatter {
         String formatted;
 
         if (!StringUtils.isEmpty(toHighlight)) {
-            //TODO: Replaces case with searchText.
-            formatted = text.replaceAll("(?i)" + toHighlight, "<font color='red'>" + toHighlight + "</font>");
+            formatted = getFormattedText(text, toHighlight);
         }
         else {
             formatted = text;
         }
 
         return formatted;
+    }
+
+    private static String getFormattedText(String text, String query) {
+        StringBuilder formatted = new StringBuilder();
+        String subString = "";
+        int queryLength = query.length();
+        int i = 0;
+
+        while (i < text.length()) {
+            if ((i + queryLength) < text.length()) {
+                subString = text.substring(i, i + queryLength);
+            }
+
+            if (((i + queryLength) < text.length()) && query.equalsIgnoreCase(subString)) {
+                formatted.append("<font color='red'>");
+                formatted.append(subString);
+                formatted.append("</font>");
+                i += (queryLength - 1);
+            }
+            else {
+                formatted.append(text.charAt(i));
+            }
+            i++;
+        }
+
+        return formatted.toString();
     }
 
     //TODO: Fix title based upon screen size.
