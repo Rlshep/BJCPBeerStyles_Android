6fc61c0375fad3bc3650f7ee25130cc4d4c99e08
Richard Sheppard <Rlshep@gmail.com>
2018-03-16 20:46:17 -0400

    updated synonyms

--- bjcp_beer_styles/src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
+++ bjcp_beer_styles/src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
@@ -2,5 +2,5 @@ package io.github.rlshep.bjcp2015beerstyles.constants;
 
 public class BjcpConstants {
     public static final String DATABASE_NAME = "BjcpBeerStyles.db";
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
 }
