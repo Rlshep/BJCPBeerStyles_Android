97498c8cf909458c4cf179c965f2a30cb737cacf
Richard Sheppard <rlshep@gmail.com>
2018-11-25 00:12:22 -0500

    change issues url

--- bjcp_beer_styles/src/main/java/io/github/rlshep/bjcp2015beerstyles/AboutActivity.java
+++ bjcp_beer_styles/src/main/java/io/github/rlshep/bjcp2015beerstyles/AboutActivity.java
@@ -24,7 +24,7 @@ public class AboutActivity extends BjcpActivity {
         body += getString(R.string.about_bjcp_begin) + " <a href='http://dev.bjcp.org'>" + getString(R.string.about_bjcp_mid) + "</a> " + getString(R.string.about_bjcp_end) + "<br><br>";
         body += getString(R.string.about_author) + " " + getString(R.string.twitter_begin) + " <a href='https://twitter.com/rlshep'>@rlshep</a><br><br>";
         body += getString(R.string.about_thanks_begin) + " <a href='https://github.com/seth-k/BJCP-styles-XML'>" + getString(R.string.about_thanks_mid) + "</a> " + getString(R.string.about_thanks_end) + "<br><br>";
-        body += getString(R.string.about_report_begin) + " <a href='https://github.com/Rlshep/BJCP2015BeerStyles/issues'>" + getString(R.string.about_report_end) + "</a><br><br>";
+        body += getString(R.string.about_report_begin) + " <a href='https://github.com/Rlshep/BJCPBeerStyles_Android/issues'>" + getString(R.string.about_report_end) + "</a><br><br>";
 
         TextView sectionsTextView = (TextView) findViewById(R.id.aboutText);
         sectionsTextView.setText(Html.fromHtml(body));
