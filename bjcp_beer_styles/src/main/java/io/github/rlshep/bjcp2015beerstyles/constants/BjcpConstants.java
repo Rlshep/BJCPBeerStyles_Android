70ead673677de97cf49345e0b7f18c1e4813a9ba
Richard Sheppard <rlshep@gmail.com>
2018-11-24 19:00:14 -0500

    seperate out common code

--- bjcp_beer_styles/src/main/java/io/github/rlshep/bjcp2015beerstyles/constants/BjcpConstants.java
+++
@@ -1,6 +0,0 @@
-package io.github.rlshep.bjcp2015beerstyles.constants;
-
-public class BjcpConstants {
-    public static final String DATABASE_NAME = "BjcpBeerStyles.db";
-    public static final int DATABASE_VERSION = 5;   // Make sure LoadBjcp project picks up this change for PRAGMA header
-}
