359a372c9856311512e7747ce5422ba1377faa6c
Richard <rlshep@gmail.com>
2019-01-21 17:24:27 -0500

    Android Version upgrade changes

--- bjcp_beer_styles/src/androidTest/java/io/github/rlshep/bjcp2015beerstyles/BookmarksTest.java
+++ bjcp_beer_styles/src/androidTest/java/io/github/rlshep/bjcp2015beerstyles/BookmarksTest.java
@@ -1,8 +1,8 @@
 package io.github.rlshep.bjcp2015beerstyles;
 
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.longClick;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeRight;
@@ -26,7 +25,7 @@ public class BookmarksTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
 
     @Test
-    public void setup_bookmarks_and_persist() {
+    public void testSetupBookmarksAndPersist() {
         onData(anything()).inAdapterView(withId(R.id.categoryListView)).atPosition(0).perform(longClick());
         onView(withId(R.id.pager)).perform(swipeLeft());    // Bookmarks
 
